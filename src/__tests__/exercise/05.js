325983cd6207bb767c588eb6d504293bc5e6a98d
prattvaidya <pratt.vaidya@gmail.com>
2021-06-03 16:45:28 -0700

    test components with HTTP requests

--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -3,11 +3,14 @@
 
 import * as React from 'react'
 // 🐨 you'll need to grab waitForElementToBeRemoved from '@testing-library/react'
-import {render, screen} from '@testing-library/react'
+import {render, waitForElementToBeRemoved, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import {build, fake} from '@jackfranklin/test-data-bot'
 // 🐨 you'll need to import rest from 'msw' and setupServer from msw/node
+import {rest} from 'msw'
+import {setupServer} from 'msw/node'
 import Login from '../../components/login-submission'
+import {handlers} from 'test/server-handlers'
 
 const buildLoginForm = build({
   fields: {
@@ -18,15 +21,27 @@ const buildLoginForm = build({
 
 // 🐨 get the server setup with an async function to handle the login POST request:
 // 💰 here's something to get you started
-// rest.post(
-//   'https://auth-provider.example.com/api/login',
-//   async (req, res, ctx) => {},
+const server = setupServer(...handlers)
+// const server = setupServer(
+//   rest.post(
+//     'https://auth-provider.example.com/api/login',
+//     async (req, res, ctx) => {
+//       if (!req.body.password)
+//         return res(ctx.status(400, ctx.json({message: 'Password is required'})))
+//       if (!req.body.username)
+//         return res(ctx.status(400, ctx.json({message: 'Username is required'})))
+//       return res(ctx.json({username: req.body.username}))
+//     },
+//   ),
 // )
 // you'll want to respond with an JSON object that has the username.
 // 📜 https://mswjs.io/
 
 // 🐨 before all the tests, start the server with `server.listen()`
 // 🐨 after all the tests, stop the server with `server.close()`
+beforeAll(() => server.listen())
+// afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
 
 test(`logging in displays the user's username`, async () => {
   render(<Login />)
@@ -35,14 +50,43 @@ test(`logging in displays the user's username`, async () => {
   userEvent.type(screen.getByLabelText(/username/i), username)
   userEvent.type(screen.getByLabelText(/password/i), password)
   // 🐨 uncomment this and you'll start making the request!
-  // userEvent.click(screen.getByRole('button', {name: /submit/i}))
+  userEvent.click(screen.getByRole('button', {name: /submit/i}))
 
   // as soon as the user hits submit, we render a spinner to the screen. That
   // spinner has an aria-label of "loading" for accessibility purposes, so
   // 🐨 wait for the loading spinner to be removed using waitForElementToBeRemoved
   // 📜 https://testing-library.com/docs/dom-testing-library/api-async#waitforelementtoberemoved
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
 
   // once the login is successful, then the loading spinner disappears and
   // we render the username.
   // 🐨 assert that the username is on the screen
+  expect(screen.getByText(username)).toBeInTheDocument()
+})
+
+test(`omitting the password results in an error`, async () => {
+  render(<Login />)
+  const {username} = buildLoginForm()
+
+  userEvent.type(screen.getByLabelText(/username/i), username)
+  // 🐨 uncomment this and you'll start making the request!
+  userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  // as soon as the user hits submit, we render a spinner to the screen. That
+  // spinner has an aria-label of "loading" for accessibility purposes, so
+  // 🐨 wait for the loading spinner to be removed using waitForElementToBeRemoved
+  // 📜 https://testing-library.com/docs/dom-testing-library/api-async#waitforelementtoberemoved
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
+  // once the login is successful, then the loading spinner disappears and
+  // we render the username.
+  // 🐨 assert that the username is on the screen
+  // expect(screen.getByRole('alert')).toHaveTextContent(/password required/i)
+  expect(screen.getByRole('alert').textContent).toMatchInlineSnapshot(
+    `"password required"`,
+  )
+  // gets updated automatically if the alert message in component changes.
+  // all we need to do is press 'u' in the terminal to apply new snapshot
+
+  // screen.debug() // can be used to inspect the state of a component at any given time
 })
